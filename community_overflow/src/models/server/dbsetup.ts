b093c0e0b567dccb77baf3847e3bd3f7bc241019
6829nkhpas <parnamnkh1@gmail.com>
2025-11-09 03:09:14 +0530

    feat: implement getOrCreateDB function to manage database connection and collection creation

---
+++ community_overflow/src/models/server/dbsetup.ts
@@ -0,0 +1,32 @@
+import{db} from"../name";
+import createAnswerCollection from "./answer.collection";
+import createCommentCollection from"./comment.collection";
+import createQuestionCollection from"./question.collection";
+import createVoteCollection from"./vote.collection";
+
+import {databases} from"./config";
+
+export default async function getOrCreateDB(){
+    try {
+        await databases.get(db);
+        console.log("Database Connected");
+    } catch (error) {
+        console.error("Error connecting to database:", error)
+        try {
+            await databases.create(db,db);
+            console.log("Database Created");
+            await Promise.all([
+                createQuestionCollection(),
+                createAnswerCollection(),
+                createCommentCollection(),
+                createVoteCollection(),
+            ]);
+            console.log("All Collections Created");
+            console.log("Database Connected");
+        }
+        catch (error) {
+            console.error("Error creating database or collections:",error);
+        }
+    }
+    return databases
+}
