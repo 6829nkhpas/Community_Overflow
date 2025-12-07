c4b311538560b2db7ba07d3f0c3a4c4d7befb26b
6829nkhpas <parnamnkh1@gmail.com>
2025-12-07 22:56:10 +0530

    feat: enhance Auth store with user preferences and session management methods

--- community_overflow/src/store/Auth.ts
+++ community_overflow/src/store/Auth.ts
@@ -4,5 +4,45 @@ import {persist} from 'zustand/middleware';
 
 import { AppwriteException,ID,Models } from 'node-appwrite';
 import { account } from '@/models/client/config';
+import { Inter } from 'next/font/google';
 
-export interface A
+export interface UserPrefs{
+  reputation: number;
+}
+
+interface IAuthstate {
+  user: Models.User<UserPrefs> | null;
+  session: Models.Session | null;
+  jwt: string | null;
+  hydrate: boolean;
+
+  setHydrate(): void;
+  verifySession(): Promise<void>;
+    login(email: string, password: string): Promise<{sucess: boolean; error?: AppwriteException}>;
+    createAccount(email: string, password: string, name: string): Promise<{sucess: boolean; error?: AppwriteException}>;
+    logout(): Promise<void>;
+    
+}
+export const useAuthStore = create<IAuthstate>()(
+  persist(
+    immer(set) => ({
+        user: null,
+        session: null,
+        jwt: null,
+        hydrate: false,
+
+        setHydrate() {
+            set({hydrate: true});
+    },
+
+        async verifySession() {
+            try {
+                const session = await account.getSession('current');
+                set({session});
+        }
+        catch (error) {
+                console.log(error)
+            }
+    },
+        async login(email: string, password: string) {
+)
